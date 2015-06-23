780de71f24df68fd7ccf4772ff1ab943f5632e0f
Florian Sittel <sittel@lettis.net>
2015-06-23 10:28:16 +0200

    reading cov matrix, comments

--- transs.cpp
+++ transs.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <boost/program_options.hpp>
+#include <set>
 #include <omp.h>
 
 #include "coords_file/coords_file.hpp"
@@ -11,13 +12,15 @@ int main(int argc, char* argv[]) {
   namespace po = boost::program_options;
   po::variables_map args;
   po::options_description opts(std::string(argv[0]).append(
-    "\n\n"
+    " [OPTIONS] -i FILE [PC1 [PC2 ...]]\n\n"
     "compute transfer entropies between principal components"));
   opts.add_options()
+//TODO add PC ids as positional arguments
     // required options
     ("input,i", po::value<std::string>()->required(), "principal components (required).")
     // optional parameters
     ("output,o", po::value<std::string>()->default_value(""), "output file (default: stdout)")
+    ("covmatrix,C", po::value<std::string>(), "PCA covariance matrix (for PC sigmas)")
     ("nthreads,n", po::value<unsigned int>()->default_value(0), "number of parallel threads (default: 0 == read from OMP_NUM_THREADS)")
     ("help,h", po::bool_switch()->default_value(false), "show this help.");
   try {
@@ -40,6 +43,27 @@ int main(int argc, char* argv[]) {
   }
   Tools::IO::set_out(args["output"].as<std::string>()); 
   std::string fname_input = args["input"].as<std::string>();
+  //TODO: get PC ids from args
+  std::set<std::size_t> pcs;
+
+  std::vector<double> sigmas;
+  if (args.count("covmatrix")) {
+    // load sigmas from covariance matrix
+    CoordsFile::FilePointer fh = CoordsFile::open(fname_input, "r");
+    std::size_t i=1;
+    while ( ! fh->eof()) {
+      //TODO consider if PC should be read
+      std::vector<float> line = fh->next();
+      if (line.size() > 0) {
+        // sigma_i = sqrt(cov_{ii})
+        sigmas.push_back(sqrt(line[i]));
+        ++i;
+      }
+    }
+  } else {
+    // no covariance matrix available: compute sigmas from data
+    //TODO
+  }
 
   //TODO open coords
 

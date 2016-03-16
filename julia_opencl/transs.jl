d0bbfbcf39a79e35fc3de154ad69cacb132cdef8
Florian Sittel <sittel@lettis.net>
2016-03-16 20:53:58 +0100

    probs kernel

--- julia_opencl/transs.jl
+++ julia_opencl/transs.jl
@@ -53,6 +53,10 @@ function main()
       arg_type = Int
       default = 0
       help = "lagtime (in # frames; default: 1"
+    "--wgsize"
+      arg_type = Int
+      default = 32
+      help = "workgroup size for OpenCL kernels; default: 32"
   end
   args = ArgParse.parse_args(args_settings)
 
@@ -82,16 +86,47 @@ function main()
       return data
     end
 
+    function silvermans_rule(data)
+      (nrows, ncols) = size(data)
+      bandwidths = zeros(ncols)
+      n_scaled = 1.06 * nrows^(-0.2)
+      for i in 1:ncols
+        bandwidths[i] = std(data[:,i]) * n_scaled
+      end
+      return bandwidths
+    end
+
     kernel_src = "
-      __kernel void transs(__global const float* i
-                         , __global const float* j
-                         , __global float* result) {
-     }
+      /* probabilities from Epanechnikov kernel */
+      __kernel void probs(__global const float* data
+                        , __global float* p
+                        , float ref_scaled
+                        , float h_inv_neg
+                        , int n) {
+        int gid = get_global_id[0];
+        float p_tmp = 0.0f;
+        if (gid < n) {
+          p_tmp = fma(h_inv_neg, data[gid], ref_scaled);
+          p_tmp *= p_tmp;
+          if (p_tmp <= 1) {
+            p_tmp = fma(p_tmp, -0.75, 0.75);
+          }
+        }
+        p[gid] = p_tmp;
+      }
     "
-    
+
     # wrapper for parallel processing
-    function worker(fname, ncols, partition)
+    function worker(fname, ncols, partition, wgsize)
       data = load_data(fname, ncols)
+      #TODO better bandwidth selection via Jones & Sheather?
+      #     https://www.jstor.org/stable/2345597
+      bandwidths = silvermans_rule(data)
+      # extend with empty rows to align memory to group size
+      n = size(data)[1]
+      n_workgroups = cld(n, wgsize)
+      n_extended = n_workgroups * wgsize
+      data = [data ; zeros(n_extended - n, ncols)]
       # setup OpenCL environment for this worker
       gpu_platform = 0
       for p in cl.platforms()
@@ -104,25 +139,53 @@ function main()
       ctx = cl.Context(gpu)
       queue = cl.CmdQueue(ctx, :profile)
       prg = cl.Program(ctx, source=kernel_src) |> cl.build!
-      reduce_krnl = cl.Kernel(prg, "reduce")
-      #global_size = (n,)
-      #local_size =  (nworkers,)
-
+      # setup kernels
+      probs_krnl = cl.Kernel(prg, "probs")
+      # setup buffers
+      i_buf = cl.Buffer(Float32, ctx, :r, n_extended)
+      j_buf = cl.Buffer(Float32, ctx, :r, n_extended)
+      p_tau_buf = cl.Buffer(Float32, ctx, :rw, n_extended)
+      p_prev_buf = cl.Buffer(Float32, ctx, :rw, n_extended)
+      p_buf = cl.Buffer(Float32, ctx, :rw, n_extended)
       # compute transfer entropies on this gpu
       T = Dict()
       for (i,j) in sort(partition)
-        i_buf = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=data[:,i])
-        j_buf = cl.Buffer(Float32, ctx, (:r, :copy), hostbuf=data[:,j])
+        cl.write!(queue, i_buf, data[:,i])
+        cl.write!(queue, j_buf, data[:,j])
+        # compute kernel evaluations per k for different prob-values
+        for k in tau:n
+          probs_krnl[queue, (n_extended), wgsize]
+                      (i_buf
+                     , p_tau_buf
+                     , data[k-tau,i]/bandwidths[i]
+                     , 1/bandwidths[i]
+                     , n)
+          probs_krnl[queue, (n_extended), wgsize]
+                      (j_buf
+                     , p_buf
+                     , data[k,j]/bandwidths[j]
+                     , 1/bandwidths[j]
+                     , n)
+          probs_krnl[queue, (n_extended), wgsize]
+                      (j_buf
+                     , p_prev_buf
+                     , data[k-1,j]/bandwidths[j]
+                     , 1/bandwidths[j]
+                     , n)
+          # TODO: reduce kernel evaluations, i.e., compute product kernel
 
-        # event = kern[queue, global_size, local_size](i_buf, j_buf, ...)
-        # event[:profile_duration]  # [s]
 
-        #TODO run kernels
-        #TODO reduce results
+          # TODO: the same, vice versa (i <-> j)
+
+
+        end
+
+        # TODO: reduce prob-values to T_ij
+
       end
       return T
     end
-  end # @everyvwhere
+  end # @everywhere
 
   # partition indices to balanced load for gpus
   load_partitioning = load_partitions(args["ncols"], ngpus)
@@ -134,7 +197,8 @@ function main()
                                          , i+1
                                          , args["input"]
                                          , args["ncols"]
-                                         , load_partitioning[i])
+                                         , load_partitioning[i]
+                                         , args["wgsize"])
     end
   end
   #TODO write results to output

665d4dad87390d63c110f7c77ad2de63b3fe9c2a
Florian Sittel <sittel@lettis.net>
2016-03-17 21:06:58 +0100

    reduction kernel

--- julia_opencl/transs.jl
+++ julia_opencl/transs.jl
@@ -97,13 +97,15 @@ function main()
     end
 
     kernel_src = "
+      #define WGSIZE %d
+
       /* probabilities from Epanechnikov kernel */
       __kernel void probs(__global const float* data
                         , __global float* p
                         , float ref_scaled
                         , float h_inv_neg
-                        , int n) {
-        int gid = get_global_id[0];
+                        , uint n) {
+        uint gid = get_global_id[0];
         float p_tmp = 0.0f;
         if (gid < n) {
           p_tmp = fma(h_inv_neg, data[gid], ref_scaled);
@@ -114,10 +116,116 @@ function main()
         }
         p[gid] = p_tmp;
       }
+
+//TODO: combine both kernel into one map-reduce step
+
+      /* reduce probabilities to partial product-kernel sums
+         by stagewise-pairwise parallel summation */
+      __kernel void reduce_probs(__global const float* p_now
+                               , __global const float* p_prev
+                               , __global const float* p_tau
+                               , __global float* P1partial
+                               , __global float* P2partial
+                               , __global float* P3partial
+                               , __global float* P4partial) {
+        __local float p_now_wg[WGSIZE];
+        __local float p_prev_wg[WGSIZE];
+        __local float p_tau_wg[WGSIZE];
+        __local float sum[WGSIZE];
+
+        uint stride;
+        uint gid = get_global_id(0);
+        uint lid = get_local_id(0);
+        uint wid = get_group_id(0);
+
+        p_now_wg[lid] = p_now[gid];
+        p_prev_wg[lid] = p_prev[gid];
+        p_tau_wg[lid] = p_tau[gid];
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        /* P1: p(p_now, p_prev, p_tau) */
+        sum[lid] = p_now_wg[lid] * p_prev_wg[lid] * p_tau_wg[lid];
+        for (stride=WGSIZE/2; stride > 0; stride /= 2) {
+          barrier(CLK_LOCAL_MEM_FENCE);
+          if (local_id < stride) {
+            sum[lid] += p_now_wg[lid+stride]
+                        * p_prev_wg[lid+stride]
+                        * p_tau_wg[lid+stride];
+          }
+        }
+        if (lid == 0) {
+          P1partial[wid] = sum[0];
+        }
+
+        /* P2: p(p_prev) */
+        sum[lid] = p_prev_wg[lid];
+        for (stride=WGSIZE/2; stride > 0; stride /= 2) {
+          barrier(CLK_LOCAL_MEM_FENCE);
+          if (local_id < stride) {
+            sum[lid] += p_prev_wg[lid+stride];
+          }
+        }
+        if (lid == 0) {
+          P2partial[wid] = sum[0];
+        }
+
+        /* P3: p(p_prev, p_tau) */
+        sum[lid] = p_prev_wg[lid] * p_tau_wg[lid];
+        for (stride=WGSIZE/2; stride > 0; stride /= 2) {
+          barrier(CLK_LOCAL_MEM_FENCE);
+          if (local_id < stride) {
+            sum[lid] += p_prev_wg[lid+stride]
+                        * p_tau_wg[lid+stride];
+          }
+        }
+        if (lid == 0) {
+          P3partial[wid] = sum[0];
+        }
+
+        /* P4: p(p_now, p_prev) */
+        sum[lid] = p_now_wg[lid] * p_prev_wg[lid];
+        for (stride=WGSIZE/2; stride > 0; stride /= 2) {
+          barrier(CLK_LOCAL_MEM_FENCE);
+          if (local_id < stride) {
+            sum[lid] += p_now_wg[lid+stride]
+                        * p_prev_wg[lid+stride];
+          }
+        }
+        if (lid == 0) {
+          P4partial[wid] = sum[0];
+        }
+      }
+
+
+      __kernel void collect_partials(const float* P1partial
+                                   , const float* P2partial
+                                   , const float* P3partial
+                                   , const float* P4partial
+                                   , float* Pacc
+                                   , float* T
+                                   , uint idx_T
+                                   , uint n_workgroups) {
+        float P1 = 0.0f;
+        float P2 = 0.0f;
+        float P3 = 0.0f;
+        float P4 = 0.0f;
+        uint i;
+        for (i=0; i < n_workgroups; ++i) {
+          P1 += P1partial[i];
+          P2 += P2partial[i];
+          P3 += P3partial[i];
+          P4 += P4partial[i];
+        }
+        Pacc[0] += P1;
+        Pacc[1] += P2;
+        Pacc[2] += P3;
+        Pacc[3] += P4;
+        T[idx_T] += P1 * log2(P1*P2/P3/P4);
+      }
     "
 
     # wrapper for parallel processing
-    function worker(fname, ncols, partition, wgsize)
+    function worker(fname, ncols, partitioning, wgsize)
       data = load_data(fname, ncols)
       #TODO better bandwidth selection via Jones & Sheather?
       #     https://www.jstor.org/stable/2345597
@@ -138,51 +246,79 @@ function main()
       gpu = cl.available_devices(gpu_platform)[myid()-1]
       ctx = cl.Context(gpu)
       queue = cl.CmdQueue(ctx, :profile)
-      prg = cl.Program(ctx, source=kernel_src) |> cl.build!
+      prg = cl.Program(ctx, source=@sprintf(kernel_src, wgsize)) |> cl.build!
       # setup kernels
       probs_krnl = cl.Kernel(prg, "probs")
+      reduce_probs_krnl = cl.Kernel(prg, "reduce_probs")
       # setup buffers
       i_buf = cl.Buffer(Float32, ctx, :r, n_extended)
       j_buf = cl.Buffer(Float32, ctx, :r, n_extended)
       p_tau_buf = cl.Buffer(Float32, ctx, :rw, n_extended)
       p_prev_buf = cl.Buffer(Float32, ctx, :rw, n_extended)
       p_buf = cl.Buffer(Float32, ctx, :rw, n_extended)
+      p1_partial_buf = cl.Buffer(Float32, ctx, :rw, n_workgroups)
+      p2_partial_buf = cl.Buffer(Float32, ctx, :rw, n_workgroups)
+      p3_partial_buf = cl.Buffer(Float32, ctx, :rw, n_workgroups)
+      p4_partial_buf = cl.Buffer(Float32, ctx, :rw, n_workgroups)
+      Pacc_buf = cl.Buffer(Float32, ctx, :rw, 4)
+      T_buf = cl.Buffer(Float32, ctx, :rw, length(partitioning))
+
+      function kernel_invocation(buf1, buf2, ii, jj)
+        for k in tau:n
+          #TODO blocking vs. non-blocking kernel calls
+          #     (cl.call vs cl.enqueue_kernel)
+          probs_krnl[queue, (n_extended), wgsize]
+                      (buf1
+                     , p_tau_buf
+                     , data[k-tau,ii]/bandwidths[ii]
+                     , 1/bandwidths[ii]
+                     , n)
+
+          probs_krnl[queue, (n_extended), wgsize]
+                      (buf2
+                     , p_buf
+                     , data[k,jj]/bandwidths[jj]
+                     , 1/bandwidths[jj]
+                     , n)
+
+          probs_krnl[queue, (n_extended), wgsize]
+                      (buf2
+                     , p_prev_buf
+                     , data[k-1,jj]/bandwidths[jj]
+                     , 1/bandwidths[jj]
+                     , n)
+
+          reduce_probs_krnl[queue, (n_extended), wgsize]
+                      (p_buf
+                     , p_prev_buf
+                     , p_tau_buf
+                     , p1_partial_buf
+                     , p2_partial_buf
+                     , p3_partial_buf
+                     , p4_partial_buf)
+
+          collect_partials_krnl[queue, 1, 1]
+                      (p1_partial_buf
+                     , p2_partial_buf
+                     , p3_partial_buf
+                     , p4_partial_buf
+                     , Pacc_buf
+                     , T_buf
+                     , idx
+                     , n_workgroups)
+        end
+        #TODO retrieve T from GPU
+      end
+
       # compute transfer entropies on this gpu
-      T = Dict()
-      for (i,j) in sort(partition)
+      for (idx, (i,j)) in enumerate(partitioning)
         cl.write!(queue, i_buf, data[:,i])
         cl.write!(queue, j_buf, data[:,j])
-        # compute kernel evaluations per k for different prob-values
-        for k in tau:n
-          probs_krnl[queue, (n_extended), wgsize]
-                      (i_buf
-                     , p_tau_buf
-                     , data[k-tau,i]/bandwidths[i]
-                     , 1/bandwidths[i]
-                     , n)
-          probs_krnl[queue, (n_extended), wgsize]
-                      (j_buf
-                     , p_buf
-                     , data[k,j]/bandwidths[j]
-                     , 1/bandwidths[j]
-                     , n)
-          probs_krnl[queue, (n_extended), wgsize]
-                      (j_buf
-                     , p_prev_buf
-                     , data[k-1,j]/bandwidths[j]
-                     , 1/bandwidths[j]
-                     , n)
-          # TODO: reduce kernel evaluations, i.e., compute product kernel
-
-
-          # TODO: the same, vice versa (i <-> j)
-
-
-        end
-
-        # TODO: reduce prob-values to T_ij
-
+        kernel_invocation(i_buf, j_buf, i, j)
+        kernel_invocation(j_buf, i_buf, j, i)
+        #TODO write to T
       end
+
       return T
     end
   end # @everywhere

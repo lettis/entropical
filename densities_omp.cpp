be2b2e8ab841da5edd5d2cdd40626867ebd9520c
Florian Sittel <sittel@lettis.net>
2016-06-13 22:36:14 +0200

    finished debugging: stupid thing, test data was faulty, not code

--- densities_omp.cpp
+++ densities_omp.cpp
@@ -13,7 +13,6 @@
 
 namespace {
 
-
   float
   epanechnikov(float ref_val
              , float val
@@ -26,30 +25,32 @@ namespace {
   typedef std::vector<std::array<std::size_t, 2>> Boxes;
 
   Boxes
-  boxes(const std::vector<float>& sorted_coords
+  boxes(const float* coords
+      , std::size_t i_col
+      , const std::vector<float>& sorted_coords
       , std::size_t n_rows
       , float h) {
     Boxes bxs(n_rows);
+    float ref_val;
     std::size_t i,k;
     #pragma omp parallel for default(none)\
-                             private(i,k)\
-                             firstprivate(n_rows,h)\
-                             shared(bxs,sorted_coords)\
+                             private(ref_val,i,k)\
+                             firstprivate(n_rows,h,i_col)\
+                             shared(bxs,sorted_coords,coords)\
                              schedule(dynamic,8)
     for (i=0; i < n_rows; ++i) {
+      ref_val = coords[i_col*n_rows+i];
       // find min
-      bxs[i][0] = i;
-      for (k=i; k > 0; --k) {
-        if (sorted_coords[i] - sorted_coords[k-1] > h) {
-          bxs[i][0] = k-1;
+      for (k=0; k < n_rows; ++k) {
+        if (ref_val - sorted_coords[k] < h) {
+          bxs[i][0] = k;
           break;
         }
       }
       // find max
-      bxs[i][1] = i;
-      for (k=i+1; k < n_rows; ++k) {
-        if (sorted_coords[k] - sorted_coords[i] > h) {
-          bxs[i][1] = k;
+      for (k=n_rows; k > 0; --k) {
+        if (sorted_coords[k-1] - ref_val < h) {
+          bxs[i][1] = k-1;
           break;
         }
       }
@@ -58,56 +59,55 @@ namespace {
   }
 
   std::vector<float>
-  densities_1d(const std::vector<float>& sorted_coords
+  densities_1d(const float* coords
+             , std::vector<std::size_t> i_col
+             , const std::vector<float>& sorted_coords
              , std::vector<float> h) {
     std::size_t n_rows = sorted_coords.size();
-    std::vector<float> P(n_rows);
+    std::vector<float> P(n_rows, 0.0f);
     std::size_t i,k;
-    float ref_val, epa;
-    bool in_range;
-//    #pragma omp parallel for default(none)\
-//                             private(ref_val,i,k,in_range,epa)\
-//                             firstprivate(n_rows,h)\
-//                             shared(sorted_coords,P)\
-//                             schedule(dynamic,1)
+    float ref_val;
+    Boxes bxs = boxes(coords
+                    , i_col[0]
+                    , sorted_coords
+                    , n_rows
+                    , h[0]);
+    #pragma omp parallel for default(none)\
+                             private(ref_val,i,k)\
+                             firstprivate(n_rows,h,i_col)\
+                             shared(coords,sorted_coords,P,bxs)\
+                             schedule(dynamic,1)
     for (i=0; i < n_rows; ++i) {
-      ref_val = sorted_coords[i];
-      in_range = false;
-      for (k=0; k < n_rows; ++k) {
-        epa = epanechnikov(ref_val, sorted_coords[k], h[0]);
-//        if (epa > 0) {
-//          std::cerr << i << " " << epa << std::endl;
-//        }
-//std::cerr << epa << std::endl;
-//        if (epa > 0.0f && (! in_range)) {
-//          in_range = true;
-//          P[i] = epa;
-//        } else if (epa > 0.0f && (in_range)) {
-          P[i] += epa;
-//        } else if (in_range) {
-//          break;
-//        }
+      ref_val = coords[i_col[0]*n_rows+i];
+      for (k=bxs[i][0]; k <= bxs[i][1]; ++k) {
+        P[i] += epanechnikov(ref_val, sorted_coords[k], h[0]);
       }
-//      P[i] /= n_rows;
+      P[i] /= n_rows;
     }
     return P;
   }
   
   std::vector<float>
-  densities_2d(const std::vector<float>& sorted_coords
+  densities_2d(const float* coords
+             , std::vector<std::size_t> i_col
+             , const std::vector<float>& sorted_coords
              , std::vector<float> h) {
     std::size_t n_rows = sorted_coords.size() / 2;
     std::vector<float> P(n_rows);
+    Boxes bxs = boxes(coords
+                    , i_col[0]
+                    , sorted_coords
+                    , n_rows
+                    , h[0]);
     std::size_t i,k;
     float ref_1, ref_2;
-    Boxes bxs = boxes(sorted_coords, n_rows, h[0]);
     #pragma omp parallel for default(none)\
                              private(ref_1,ref_2,i,k)\
-                             firstprivate(n_rows,h)\
-                             shared(bxs,sorted_coords,P)
+                             firstprivate(i_col,n_rows,h)\
+                             shared(bxs,sorted_coords,P,coords)
     for (i=0; i < n_rows; ++i) {
-      ref_1 = sorted_coords[i];
-      ref_2 = sorted_coords[n_rows+i];
+      ref_1 = coords[i_col[0]*n_rows+i];
+      ref_2 = coords[i_col[1]*n_rows+i];
       for (k=bxs[i][0]; k <= bxs[i][1]; ++k) {
         P[i] += epanechnikov(ref_1, sorted_coords[k], h[0])
               * epanechnikov(ref_2, sorted_coords[n_rows+k], h[1]);
@@ -118,21 +118,27 @@ namespace {
   }
   
   std::vector<float>
-  densities_3d(const std::vector<float>& sorted_coords
+  densities_3d(const float* coords
+             , std::vector<std::size_t> i_col
+             , const std::vector<float>& sorted_coords
              , std::vector<float> h) {
     std::size_t n_rows = sorted_coords.size() / 3;
     std::vector<float> P(n_rows);
+    Boxes bxs = boxes(coords
+                    , i_col[0]
+                    , sorted_coords
+                    , n_rows
+                    , h[0]);
     std::size_t i,k;
     float ref_1, ref_2, ref_3;
-    Boxes bxs = boxes(sorted_coords, n_rows, h[0]);
     #pragma omp parallel for default(none)\
                              private(ref_1,ref_2,ref_3,i,k)\
-                             firstprivate(n_rows,h)\
-                             shared(bxs,sorted_coords,P)
+                             firstprivate(i_col,n_rows,h)\
+                             shared(bxs,sorted_coords,P,coords)
     for (i=0; i < n_rows; ++i) {
-      ref_1 = sorted_coords[i];
-      ref_2 = sorted_coords[n_rows+i];
-      ref_3 = sorted_coords[2*n_rows+i];
+      ref_1 = coords[i_col[0]*n_rows+i];
+      ref_2 = coords[i_col[1]*n_rows+i];
+      ref_3 = coords[i_col[2]*n_rows+i];
       for (k=bxs[i][0]; k <= bxs[i][1]; ++k) {
         P[i] += epanechnikov(ref_1, sorted_coords[k], h[0])
               * epanechnikov(ref_2, sorted_coords[n_rows+k], h[1])
@@ -160,11 +166,20 @@ combined_densities(const float* coords
                                                              , i_col);
   switch(n_dim) {
     case 1:
-      return densities_1d(sorted_coords, h);
+      return densities_1d(coords
+                        , i_col
+                        , sorted_coords
+                        , h);
     case 2:
-      return densities_2d(sorted_coords, h);
+      return densities_2d(coords
+                        , i_col
+                        , sorted_coords
+                        , h);
     case 3:
-      return densities_3d(sorted_coords, h);
+      return densities_3d(coords
+                        , i_col
+                        , sorted_coords
+                        , h);
     default:
       // this should never happen!
       exit(EXIT_FAILURE);

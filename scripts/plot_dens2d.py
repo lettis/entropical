aa215e9b0b3ec55c79ea29ab88db3a8027677733
Florian Sittel <sittel@lettis.net>
2016-06-14 22:49:59 +0200

    improved dens-plotting scripts

--- scripts/plot_dens2d.py
+++ scripts/plot_dens2d.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
-from default_definitions import *
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
 
-dens = to_array(sys.argv[1])
+dens = pd.read_table(sys.argv[1]
+                   , sep=" "
+                   , skipinitialspace=True)
+dens = np.array(dens)
 dens = dens.reshape(int(dens.size/3), 3)
 
 plt.scatter(dens[:,0]
           , dens[:,1]
-          , color=plt.cm.Greys(dens[:,2]/max(dens[:,2]))
-          , marker='.')
+          , color=plt.cm.inferno(dens[:,2]/max(dens[:,2]))
+          , marker='.'
+          , s=0.3)
 plt.show()
 
--- scripts/plot_dens3d.py
+++ scripts/plot_dens3d.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
-from default_definitions import *
+import pandas as pd
+import sys
+import numpy as np
 
-#TODO
-#from mpl_toolkits.mplot3d import Axes3D
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-dens = to_array(sys.argv[1])
-dens = dens.reshape(int(dens.size/3), 3)
+dens = pd.read_table(sys.argv[1]
+                   , sep=" "
+                   , skipinitialspace=True)
+dens = np.array(dens)
+dens = dens.reshape(int(dens.size/4), 4)
 
-plt.scatter(dens[:,0]
-          , dens[:,1]
-          , color=plt.cm.Greys(dens[:,2]/max(dens[:,2]))
-          , marker='.')
+Axes3D.scatter(dens[:,0]
+             , dens[:,1]
+             , dens[:,2]
+             , color=plt.cm.inferno(dens[:,3]/max(dens[:,3]))
+             , depthshade=True
+             , marker='.'
+             , s=0.3)
 plt.show()
 
